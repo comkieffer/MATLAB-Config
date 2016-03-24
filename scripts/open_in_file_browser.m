b96b78fad83b970fcf57a3e36056acae854deae0
com_kieffer <comkieffer.me@gmail.com>
2016-03-24 14:32:46 +0100

    Suppress output of `xdg-open`  in `open_in_file_browser`
    redirect stdout and stderr to `/dev/null` to suppress output. Currenttly supports bash-like shells and fish shell.

--- scripts/open_in_file_browser.m
+++ scripts/open_in_file_browser.m
@@ -23,9 +23,24 @@ function open_in_file_browser(path)
         warning('%s is not a folder', path)
         return
     end
+
+    % To properly redirecet stdout and stderr we need to detect the current
+    % shell. Currently we support bash/sh and fish. 
+    output_redirect_syntax = '> /dev/null 2>&1';
+    if strcmp(shell(), 'fish')
+        output_redirect_syntax = '> /dev/null ^&1';
+    end
     
-    [status, stdout] = system(['xdg-open ', path]);
+    [status, stdout] = system(['xdg-open ', path, ' ', output_redirect_syntax]);
     if status ~= 0
         warning('xdg-open failed.\n  Status code: %i\n  stdout: %s', status, stdout);
     end
+end
+
+function shellname = shell()    
+    % Use -n to not emit a '\n' charracter
+    [status, out] = system('echo -n $SHELL');
+    
+    if status ~= 0; warning('Unable to detect shell type.'); end
+    [~, shellname, ~] = fileparts(out);
 end
